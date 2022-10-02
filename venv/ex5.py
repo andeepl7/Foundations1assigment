021c2eb06326cef9e52f8791a933b7c4f0426c66
Andee <andrea.perez.lop90@gmail.com>
2022-10-02 18:03:35 +0200

    Actualizando

--- venv/ex5.py
+++ venv/ex5.py
@@ -3,51 +3,12 @@ Assignment II
 """
 import random
 
-
 class UniqueId:
     unique_id = 100000
 
     def get_next_id(self):
         UniqueId.unique_id += 1
         return UniqueId.unique_id
-
-
-def menu():
-    print('Welcome to the CBS manage system for the Diploma (Minor) programme for Data Science, '
-          '\n please choose number to continue: ')
-    print('1 to Add a Course')
-    print('2 to Delete a Course')
-    print('3 to Create a student')
-    print('4 to Add an Student to a course')
-    print('5 to Remove an Student from a course')
-    print('6 to Grade an assignment')
-    print('7 to Check the status of your course')
-    print('8 to Check students list with grades')
-    print('9 to quit')
-
-
-def main():
-    menu()
-    t = int(input('Enter menu choice: '))
-
-    if t == 1:
-        add_course()
-    elif t == 2:
-        remove_course()
-    elif t == 3:
-        option3()
-    elif t == 4:
-        option4()
-    elif t == 5:
-        option5()
-    elif t == 6:
-        option6()
-    elif t == 7:
-        option7()
-    else:
-        pass
-    main()
-
 
 class DiplomaProgramme:
     def __init__(self):
@@ -56,6 +17,9 @@ class DiplomaProgramme:
                         "Visual Analytics": Course("Visual Analytics"),
                         "Text Analytics": Course("Text Analytics")
                         }
+        self.students = {}
+        self.pass_limit = 12
+        self.pass_distinction_limit = 16
 
     def add_course(self, course_name):
         if course_name in self.courses.keys():
@@ -69,14 +33,45 @@ class DiplomaProgramme:
         else:
             self.courses.pop(course_name)
 
+    def print_courses(self):
+        print(self.courses.keys())
+
     def list_courses(self):
-        print (self.courses.keys())
+        return self.courses
 
-    def list_students_passed(self):
-        pass
+    def register_students(self, students):
+        for student in students:
+            self.students[student.get_id()] = student
+            # Automatically add student to diploma´s courses
+            for course in self.courses.keys():
+                self.courses[course].add_student(student)
 
-    def list_students_distinction(self):
-        pass
+    def list_students(self):
+        print("Diploma Students")
+        for student_id in self.students.keys():
+            print(student_id,
+                  self.students[student_id].name,
+                  self.students[student_id].lastname)
+
+    def diploma_scores(self):
+        students_scores = {}
+        for student_id, student in self.students.items():
+            assignment_score = 0
+            # Calculating student score by iterating through each course
+            for course in student.courses_enrolled():
+                # Get Grades for student, per course enrolled
+                assignment_score += student.student_grades(course).count("PASS")
+            students_scores[student.get_id()] = self.score_description(assignment_score)
+        print("Assignment Score")
+        print(students_scores)
+
+    def score_description(self,score):
+        if score > self.pass_distinction_limit:
+            return 'PASS with distinction'
+        if score > self.pass_limit:
+            return 'PASS'
+        return 'FAIL'
+
 
 
 class Student:
@@ -87,10 +82,10 @@ class Student:
         self.id = self.create_id()
         # {"Course1": [PASS, FAIL, PASS, PASS],
         #  "Course2": [PASS, PASS, PASS]}
-        self.courses = {}
+        self.courses_grades = {}
 
-    def enroll_to_course(self, course):
-        self.courses[course.name] = course.assignments
+    def enroll_to_course(self, course_name):
+        self.courses_grades[course_name] = ["UNGRADED", "UNGRADED", "UNGRADED", "UNGRADED", "UNGRADED"]
 
     def get_id(self):
         return self.id
@@ -102,16 +97,24 @@ class Student:
         return full_name_reduced + str(u.get_next_id())
 
     def courses_enrolled(self):
-        print ( )
+        return list(self.courses_grades.keys())
 
-    def student_grades(self):
-        pass
+    def print_student_grades(self):
+        for course in self.courses_grades.keys():
+            print(f"Course {course}: {self.courses_grades[course]}")
+
+    def student_grades(self, course_name):
+        return self.courses_grades[course_name]
 
     def student_info(self):
+        print("-"*20)
         print('Student info: ')
         print(self.name, self.lastname, self.id)
-        # print('The student is enrol in this courses: ', courses)
-        # print('The grades of its assignments are:')
+        print('The student is enrolled in this courses: ')
+        for enrolled_course in self.courses_enrolled():
+            print(f'Assignment grades/{enrolled_course}')
+            print(self.student_grades(enrolled_course))
+        print("-"*16)
 
 
 class Course:
@@ -119,19 +122,19 @@ class Course:
         self.course_name = course_name
         self.students_enrolled = []
 
-    def add_student(self, student_id, diploma_student_list):
+    def get_student(self, student_id):
+        for course_student in self.students_enrolled:
+            if student_id == course_student.get_id():
+                return course_student
+        print(f"Student with student_id {student_id} not found")
+        return None
 
-        # List of Dictionary of students:
-        # Key : Value
-        # student_id: student_object
-        diploma_students = [dict({s.get_id(): s}) for s in diploma_student_list]
-
-        for diploma_student in diploma_students:
-            if student_id in diploma_student.keys():
-                if diploma_student[student_id] in self.students_enrolled:
-                    raise Exception('The student is already assigned')
-                else:
-                    self.students_enrolled.append(diploma_student[student_id])
+    def add_student(self, student):
+        if student in self.students_enrolled:
+            raise Exception('The student is already assigned')
+        else:
+            self.students_enrolled.append(student)
+            student.enroll_to_course(self.course_name)
 
     def remove_student(self, student_id):
         for student in self.students_enrolled:
@@ -143,45 +146,94 @@ class Course:
             a.student_info()
         # print(self.students_enrolled)
 
-    def grade_student(self, student_id, assignment_number, grade):
-        if type(grade) is not string:
-            raise Exception("The grade could only be Pass or Fail")
-        if assignment_number > 5:
-            raise Exception('The course cannot has more than 5 assignments')
-        if student_id in diploma_student_list:
-            self.assigment_number = assignment_number
-            self.grade = grade
-        else:
-            raise Exception('The student is not enrol in this course')
+    def grade_student(self, student_id):
+        student = self.get_student(student_id)
+        if student is not None:
+            student_grades = student.courses[self.course_name]
+            assignment_score = 0
+            for grade in student_grades:
+                if grade == "PASS":
+                    assignment_score += 1
+            return assignment_score, self.pass_course(assignment_score)
 
+    def pass_course(self, assignment_score):
+        if assignment_score >= 3:
+            return True
+        return False
+
+
+    def grade_assignments(self, student_id, grades):
+        student = self.get_student(student_id)
+        if student is not None:
+            student.courses_grades[self.course_name] = grades
+
+    def get_student_grades(self, student_id):
+        student = self.get_student(student_id)
+        if student is not None:
+            return student.courses_grades[self.course_name]
 
 # Testing section
+grades_test_pass = ["PASS", "FAIL", "PASS", "PASS", "PASS"]
+grades_test_fail = ["PASS", "FAIL", "FAIL", "FAIL", "PASS"]
+grades_test_pass_distinction = ["PASS", "PASS", "PASS", "PASS", "PASS"]
 
 def test_diploma():
     diploma = DiplomaProgramme()
-    diploma.list_courses()
+    diploma.print_courses()
     diploma.add_course("Big Data")
     diploma.add_course("Cybersecurity")
-    diploma.list_courses()
+    diploma.print_courses()
     diploma.remove_course("Big Data")
-    diploma.list_courses()
+    diploma.print_courses()
     diploma.add_course("Cybersecurity")
     diploma.remove_course("Storytelling")
-    diploma.list_courses()
+    diploma.print_courses()
 
+def test_diploma_student(students):
+    diploma = DiplomaProgramme()
+    diploma.register_students(students)
+    diploma.list_students()
+    print("Courses per Student")
+    for s in students:
+        print(s.get_id(), s.courses_enrolled())
+    for course_name, course in diploma.list_courses().items():
+        course.grade_assignments('ANPE100002', grades_test_fail)
+        course.grade_assignments('DACA100003', grades_test_pass_distinction)
+        course.grade_assignments('ALCA100001', grades_test_pass)
+    diploma.diploma_scores()
 
 def test_course_class(course_students):
-    course_name = "Maths"
-    my_course = Course(course_name=course_name)
-    my_course.add_student('ANPE071290', course_students)
-    my_course.add_student('ALCA100795', course_students)
-    my_course.remove_student('ALCA100795')
-    my_course.all_students()
+    python_programming = Course(course_name="Python Programming")
+    for student in course_students:
+        python_programming.add_student(student)
+    python_programming.grade_assignments('ANPE100002', grades_test_fail)
+    python_programming.grade_assignments('DACA100003', grades_test_pass_distinction)
+    python_programming.grade_assignments('ALCA100001', grades_test_pass)
 
+    data_mining = Course(course_name="Data Mining and Machine Learning")
+    for student in course_students:
+        data_mining.add_student(student)
+    data_mining.grade_assignments('ANPE100002', grades_test_fail)
+    data_mining.grade_assignments('DACA100003', grades_test_pass_distinction)
+    data_mining.grade_assignments('ALCA100001', grades_test_pass)
+
+    visual_analytics = Course(course_name="Visual Analytics")
+    for student in course_students:
+        visual_analytics.add_student(student)
+    visual_analytics.grade_assignments('ANPE100002', grades_test_fail)
+    visual_analytics.grade_assignments('DACA100003', grades_test_pass_distinction)
+    visual_analytics.grade_assignments('ALCA100001', grades_test_pass)
+
+    text_analytics = Course(course_name="Text Analytics")
+    for student in course_students:
+        text_analytics.add_student(student)
+    text_analytics.grade_assignments('ANPE100002', grades_test_fail)
+    text_analytics.grade_assignments('DACA100003', grades_test_pass_distinction)
+    text_analytics.grade_assignments('ALCA100001', grades_test_pass)
+    #course_students[0].student_info()
 
 
 if __name__ == '__main__':
-    num_students = 1000
     student_info = [
         {"name": "Alessandro", "surname": "Catania", "dob": "100795"},
         {"name": "Andrea", "surname": "Perez", "dob": "071290"},
@@ -193,8 +245,11 @@ if __name__ == '__main__':
         s = Student(name=student["name"],
                     lastname=student["surname"],
                     dob=student["dob"])
-        s.student_info()
+        #s.student_info()
         students.append(s)
 
-    test_course_class(students)
-    test_diploma()
+    test_diploma_student(students)
+    #test_course_class(students)
+
+    for s in students:
+        s.student_info()
